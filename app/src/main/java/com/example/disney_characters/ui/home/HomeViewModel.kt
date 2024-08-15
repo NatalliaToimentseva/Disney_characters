d67fd83a9a8d280a4e82c6681cd40c67cf9cde9c
Natalia_Toimentseva <NatalliaToimentseva@gmail.com>
2024-08-15 16:45:42 +0300

    With mock

--- app/src/main/java/com/example/disney_characters/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/disney_characters/ui/home/HomeViewModel.kt
@@ -11,12 +11,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+private const val TEMP_URL =
+    "https://static.wikia.nocookie.net/disney/images/1/15/Arianna_Tangled.jpg/revision/latest?cb=20160715191802"
+
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val repository: DisneyCharactersListRepository
 ) : ViewModel() {
 
-    val disneyCharactersList = MutableLiveData<List<CharacterItemModel>>()
+    val disneyCharactersList = MutableLiveData<List<CharacterItemModel>>(
+        arrayListOf(
+            CharacterItemModel(1, "Test1", TEMP_URL),
+            CharacterItemModel(2, "Test2", TEMP_URL),
+            CharacterItemModel(3, "Test3", TEMP_URL),
+            CharacterItemModel(4, "Test4", TEMP_URL),
+            CharacterItemModel(5, "Test5", TEMP_URL),
+            CharacterItemModel(6, "Test6", TEMP_URL),
+            CharacterItemModel(7, "Test7", TEMP_URL)
+        )
+    )
     val isInProgress = MutableLiveData(false)
     var error = MutableLiveData<String?>(null)
 
